1edb3c79d5d3bcc149e74bd68142458e18af2834
Naufal Arif <101627281+Nopall-Donat@users.noreply.github.com>
2025-11-19 06:54:03 +0700

    v2 update with validator and chalk for message output

--- contact_app/v2/contact.js
+++ contact_app/v2/contact.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
 
 const dirPath = './data';
 if(!fs.existsSync(dirPath)) {
@@ -17,13 +19,42 @@ const saveContact = (name, age, email, mobilePhone) => {
 
     fs.readFile(filePath, 'utf-8', (err, data) => {
         const contacts = err ? [] : JSON.parse(data); // jika error kembalikan array kosong
+        
+        const duplicated = contacts.find(contact => contact.name === newContact.name);
+        if(duplicated) {
+            console.log(chalk.bgRed.whiteBright('Contact sudah terdaftar, gunakan nama lain!'));
+            return false; 
+        }
+        if(age || age !== null) {
+            if(age == 0) {
+                console.log(chalk.bgRed.whiteBright('Umur tidak boleh 0!'));
+                return false;
+            }
+            // validator.isNumeric() expects a string. Convert age to string first.
+            if(!validator.isNumeric(String(age))) {
+                console.log(chalk.bgRed.whiteBright('Umur harus berupa angka!'));
+                return false;
+            }
+        }
+        if(email) {
+            if(!validator.isEmail(email)) {
+                console.log(chalk.bgRed.whiteBright('Email tidak valid!'));
+                return false;
+            }
+        }
+        if(mobilePhone) {
+            if(!validator.isMobilePhone(mobilePhone, 'id-ID')) {
+                console.log(chalk.bgRed.whiteBright('Nomor telepon tidak valid!'));
+                return false;
+            }
+        }
+        
         contacts.push(newContact); // lakukan push ke contact yang bersifat array dari readFile si contact.json dengan isi dari newContact baru.
 
         // fungsi async write file harus ada di dalam sini, karena dia async jadi bisa bugg menulis dilakukan duluan dibanding baca. untuk mempertimbangkan itu, fungsinya dimasukkan kedlam proses read sebagai callback.
         fs.writeFile(filePath, JSON.stringify(contacts, null, 4), err => { // 4 ini sebagai 4 kali space untuk kerapihan file json
             if (err) throw err;
-            console.log('Data berhasil disimpan');
-            rl.close();
+            console.log(chalk.bgGreen.white.bold('Data berhasil disimpan'));
         });
     });
 }
